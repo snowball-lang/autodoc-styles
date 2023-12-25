14961303391703729252e68657316f9908ecf027
Mauro Baladés <mauro.balades@tutanota.com>
2023-12-25 09:54:05 +0000

    Replace break lines with new lines

--- md2html.js
+++ md2html.js
@@ -2,7 +2,7 @@ function md2html() {
     let selectors = document.querySelectorAll(".doc p");
     for (let i = 0; i < selectors.length; i++) {
         let selector = selectors[i];
-        let text = selector.innerHTML;
+        let text = selector.innerHTML.replace(/<br>/g, "\n").replace(/<br\/>/g, "\n");
         const html = converter.makeHtml(text);
         selector.innerHTML = html;
     }
